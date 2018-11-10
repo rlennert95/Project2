bb681272d359e5ed25f3f974657e87b69feab2aa
GodZkotm54 <41457922+rlennert95@users.noreply.github.com>
2018-11-10 12:48:21 -0500

    jawsdb installed

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,14 @@
+var mysql = require("mysql");
+var connection;
+
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection  = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
+        host: 'op2hpcwcbxb1t4z9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        port: 3306,
+        user: 'my8h9q7qlmnpqpz8',
+        password: 'gmyedys86gawwiy7',
         database: 'JAWSDB_URL'
     });
 };
